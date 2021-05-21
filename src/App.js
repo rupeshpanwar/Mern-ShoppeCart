9d5cd431031c27256967f43a1fbc954e1b9d0c20
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-05-21 06:03:57 +0530

    React setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
-import Header from '../src/components/Header'
-import Footer from '../src/components/Footer'
 
-import './bootstrap.min.css'
 
-function App() {
+
+
+const App = () => {
   return (
     <>
-      <Header />
-      <main className="py-3">
-        <Container>
-          <h1> Welcome to ProShop</h1>
-        </Container>
-      </main>
-      <Footer />
+      <h1>Welcome to Proshop home page</h1>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
