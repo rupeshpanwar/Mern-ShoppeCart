2b1467cf33a9ffc88b1141ae5af84745d8c2daa9
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-05-25 16:53:12 +0530

    store reducer action homescreen message

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import './bootstrap.min.css'
+
+//redux related 
+import { Provider } from 'react-redux'
+import store from './store'
+
 
 import './bootstrap.min.css'
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>
+  ,
   document.getElementById('root')
 );
 
